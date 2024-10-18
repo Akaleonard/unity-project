e34fc71057720bf3cbab7061375881e3d1a7cbf4
Brian <brian12leonard@gmail.com>
2024-10-18 10:25:31 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 10f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+    private BoxCollider2D boxCollider;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 newVector = new Vector2(moveHorizontal * moveSpeed, rb.linearVelocity.y);
+
+        rb.linearVelocity = newVector;
+    }
+
+    void Jump()
+    {
+        isGrounded = IsGrounded();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return boxCollider.IsTouchingLayers(groundLayer);
+    }
+}
