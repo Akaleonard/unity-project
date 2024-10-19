aa15a41375bb930e8803d65248eceb470a9dbd28
Brian <brian12leonard@gmail.com>
2024-10-19 12:58:41 -0700

    variable jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] private float jumpCutMultiplier = 0.5f;
+    [SerializeField] private float maxJumpTime = 0.3f;
     public LayerMask groundLayer;
 
+    private float jumpTimeCounter;
+    private bool isJumping;
     private Rigidbody2D rb;
     private bool isGrounded = false;
     private BoxCollider2D boxCollider;
@@ -19,6 +23,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Jump();
+
     }
 
     void FixedUpdate()
@@ -41,7 +46,42 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            isJumping = true;
+            jumpTimeCounter = maxJumpTime;
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            Debug.Log("Jump started");
+        }
+
+        // If the jump button is held and the player is still within the max jump time, continue jumping
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.AddForce(Vector2.up * (jumpSpeed * Time.deltaTime), ForceMode2D.Force);
+                jumpTimeCounter -= Time.deltaTime;
+                Debug.Log("Jumping, time left: " + jumpTimeCounter);
+            }
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+        // If the player releases the jump button early, cut the jump short
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Jump button released");
+            if (rb.linearVelocity.y > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+                Debug.Log("Jump cut");
+            }
+            isJumping = false;
+        }
+
+        if (isGrounded)
+        {
+            isJumping = false;
         }
     }
 
@@ -54,6 +94,6 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("Player has died!");
 
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(-19.32f, 7.06f, 0);
     }
 }
